d930ae5a4e464f68dbddd57bf12b9f8a02ed9517
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-01 16:35:54 +0530

    solved problem-02

--- src/generics/problem-02.ts
+++ src/generics/problem-02.ts
@@ -1,2 +1,5 @@
 // Problem Statement:
 // Write a function createPair that takes two arguments of any type and returns a tuple with those values.
+export function createPair<T, U>(a: T, b: U): [T, U] {
+  return [a, b];
+}
