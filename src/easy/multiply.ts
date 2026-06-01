792127a5e0e0fd5be49686e1e53ff6ade4b84dd0
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-01 14:13:17 +0530

    solved multiply problem

--- src/easy/multiply.ts
+++ src/easy/multiply.ts
@@ -2,3 +2,9 @@
 // Ensure the functions have proper type annotations.
 // Example Input: f1 = () => 2, f2 = () => 3
 // Example Output: 6
+
+export function multiplyResults(f1: () => number, f2: () => number): number {
+  let a = f1();
+  let b = f2();
+  return a * b;
+}
