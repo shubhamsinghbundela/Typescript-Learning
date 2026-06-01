a649c8eec98751e97ea09ae7bf3e55bbf93a6bca
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-01 15:55:03 +0530

    Solved union problem

--- src/medium/union.ts
+++ src/medium/union.ts
@@ -17,4 +17,25 @@
 
 // 78.53981633974483
 
+type Circle = {
+  radius: number;
+};
 
+type Rectangle = {
+  width: number;
+  height: number;
+};
+
+export type Shape = Circle | Rectangle;
+
+export function getArea(shape: Shape): number {
+  if ("radius" in shape) {
+    return Math.PI * shape.radius * shape.radius;
+  }
+
+  if ("width" in shape && "height" in shape) {
+    return shape.width * shape.height;
+  }
+
+  throw new Error("Invalid shape");
+}
