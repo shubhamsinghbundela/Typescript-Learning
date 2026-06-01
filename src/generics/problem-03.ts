4736e25c31888fc9660c2ddb292e87cf85f61558
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-01 16:37:46 +0530

    solved problem03

--- src/generics/problem-03.ts
+++ src/generics/problem-03.ts
@@ -1,2 +1,5 @@
 // Problem Statement:
 // Write a function mergeObjects that merges two objects and returns a new object with all properties.
+export function mergeObjects<T>(obj1: T, obj2: T): T {
+  return { ...obj1, ...obj2 };
+}
