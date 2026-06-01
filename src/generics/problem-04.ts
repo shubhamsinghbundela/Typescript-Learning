504e962876693918de17e5ff96871d64fe0e7607
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-01 16:39:19 +0530

    solved sum

--- src/generics/problem-04.ts
+++ src/generics/problem-04.ts
@@ -1,2 +1,5 @@
 // Problem Statement:
 // Write a generic function sum that accepts an array of numbers and returns the sum of all the numbers.
+export function sum(arr: number[]): number {
+  return arr.reduce((acc, cur) => acc + cur, 0);
+}
